a19ccbaafeba987931b884c304cd35b39871fb14
a-4-k <khaitanaayush@gmail.com>
2021-01-29 17:06:41 +0530

    Commit resolved

--- Math.py
+++ Math.py
@@ -1,12 +1,11 @@
 def add(x,y):
     return x+y
 def subtract(x,y):
-<<<<<<< HEAD
    if x<y:
    return Error
-=======
+
    return x-y
->>>>>>> Bug456
+   Bug456
 def multiply(x,y):
     return x*y 
 def divide(x,y):
